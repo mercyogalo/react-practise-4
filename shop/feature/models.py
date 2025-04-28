09a4e7516d4c0403197509db1c0b2544cfe86059
mercyogalo <ogalomercy8@gmail.com>
2025-04-28 23:45:04 +0300

    Created the model and serializer file for the contact form

--- shop/feature/models.py
+++ shop/feature/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=250)
+    email=models.EmailField()
+    subject=models.CharField(max_length=250)
+    message=models.CharField(max_length=1000)
+    
 
+    def __str__(self):
+        return self.name + self.email
     
