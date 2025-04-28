67a838a58ab3a66b033cb40193bf2d30c2f8a332
mercyogalo <ogalomercy8@gmail.com>
2025-04-28 22:22:14 +0300

    Created a contact file and updated the hero section of the home section

---
+++ my-app/src/Contact.js
@@ -0,0 +1,18 @@
+import {Typography} from '@mui/material';
+
+
+
+
+
+export default function Contact(){
+<>
+    <Typography 
+        variant="h1"
+        sx={{ textAlign: 'center' }}
+      >
+        Contact Us
+      </Typography>
+
+</>
+    
+}
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,6 +1,7 @@
 import { Box, Typography, Grid, Paper, Stack } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import mainImage from './images/image-1.jpeg';
+import Contact from './Contact'
 
 const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2),
@@ -14,29 +15,39 @@ export default function Home() {
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           <Grid item xs={8}>
-            <Item sx={{ height: '100%', boxSizing: 'border-box' }}>
+            <Item sx={{ 
+               height: '600px',
+               width:'1000px', 
+                }}>
               <Box 
                 component="img"
                 src={mainImage}
-                sx={{ width: '100%' }}
+                sx={{ width: '1000px',
+                  height: '600px',
+                 }}
               />
             </Item>
           </Grid>
 
           <Grid item xs={4}>
-            <Stack spacing={2}>
+            <Stack spacing={0}>
               <Item>
                 <Box 
                   component="img"
                   src={mainImage}
-                  sx={{ width: '100%' }}
+                  sx={{ width: '400px',
+                    height: '300px',
+                    borderRadius:'0px',
+                   }}
                 />
               </Item>
               <Item>
                 <Box 
                   component="img"
                   src={mainImage}
-                  sx={{ width: '100%' }}
+                  sx={{ width: '400px' ,
+                    height: '300px',
+                  }}
                 />
               </Item>
             </Stack>
@@ -50,6 +61,11 @@ export default function Home() {
       >
         About Us
       </Typography>
+
+
+<Contact />
+
+
     </>
   );
 }
