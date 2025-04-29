01c2a192fa98ea41658d8fe3ac6b6f9f6ee34d4b
mercyogalo <ogalomercy8@gmail.com>
2025-04-29 23:53:22 +0300

    Created the serializer for the contact form and added all the required dependencies

--- shop/feature/serialize.py
+++ shop/feature/serialize.py
@@ -1,1 +1,9 @@
 from . models import Contact
+from rest_framework import serializers
+
+class ContactSerializer(serializers.ModelSerializer):
+    model=Contact
+    fields='__all__'
+
+
+
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "feature",
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "shop.urls"
@@ -123,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
