564e1cb2b049706ba2fa66e1dc97996cdef9e685
mercyogalo <ogalomercy8@gmail.com>
2025-05-01 18:29:10 +0300

    Updated the views function send emails on post request and added the urls

--- shop/feature/urls.py
+++ shop/feature/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from . views import ContactFormAPIView;
 
 app_name="features"
 
 urlpatterns = [
+  path('contact/inquiry/',ContactFormAPIView.as_view())
   
 ]
 
--- shop/feature/views.py
+++ shop/feature/views.py
@@ -1,15 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serialize import ContactSerializer
+from django.core.mail import send_mail
 
 
-def contact_form(request):
-    if request.method=="POST":
-        name=request.POST['name']
-        email=request.POST['email']
-        message=request.POST['message']
+class ContactFormAPIView(APIView):
+    def post(self, request):
+        serializer = ContactSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data['name']
+            email = serializer.validated_data['email']
+            subject = serializer.validated_data['subject']
+            message = serializer.validated_data['message']
+
+            serializer.save()
+
+            try:
+                send_mail(
+                    f'New message received from {name}',
+                    f'\n\n{email}\nSubject: {subject}\n\nMessage:\n{message}',
+                    'ogalomercy8@gmail.com',
+                    ['ogalomercy8@gmail.com'],  
+                    fail_silently=False
+                )
+
+                send_mail(
+                    'Message received successfully',
+                    'Thank you for reaching out to me. I will get back to you shortly.\n\n— Ogalomercy.com',
+                    'ogalomercy8@gmail.com',
+                    [email],  
+                    fail_silently=False
+                )
+
+                return Response({'message': 'Your message has been sent successfully'}, status=status.HTTP_201_CREATED)
+
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
         
-        
-        
-        return HttpResponse
-    return HttpResponse
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
