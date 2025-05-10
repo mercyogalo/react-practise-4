8ca64577aed1b5193aa6a174388f154e0789e671
mercyogalo <ogalomercy8@gmail.com>
2025-05-10 22:25:05 +0300

    Updated the footer alignment

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,16 +1,66 @@
-import { Typography } from '@mui/material';
+import { Typography , Grid, Paper} from '@mui/material';
 
 
 
 export default function Footer(){
     return (
         <>
-         <Typography 
+<Grid container spacing={2}>
+
+
+<Grid item xs={12} sm={4}>
+<Paper elevation={3} style={{padding: 16}}>
+
+<Typography 
             variant="body1" 
             sx={{ textAlign: 'center' }}
           >
             hello world
           </Typography>
+</Paper>
+</Grid>
+
+
+
+
+
+<Grid item xs={12} sm={4}>
+<Paper elevation={3} style={{padding: 16}}>
+
+<Typography 
+            variant="body1" 
+            sx={{ textAlign: 'center' }}
+          >
+            hello world
+          </Typography>
+</Paper>
+</Grid>
+
+
+
+
+
+<Grid item xs={12} sm={4}>
+<Paper elevation={3} style={{padding: 16}}>
+<Typography 
+            variant="body1" 
+            sx={{ textAlign: 'center' }}
+          >
+            hello world
+          </Typography>
+
+</Paper>
+</Grid>
+
+
+
+
+
+
+
+</Grid>
+
+
         
         </>
 
